bbc920236241b06ad2ea3c7ea291bb47e51b20f7
Your Name <you@example.com>
2020-11-26 12:14:58 +0100

    segunda actualizacion

--- build/server.js
+++ build/server.js
@@ -1,20 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const pruebaRoutes_1 = require("./routes/pruebaRoutes");
+const futbolRoutes_1 = require("./routes/futbolRoutes");
+const identificacionRoutes_1 = require("./routes/identificacionRoutes");
 class Server {
     constructor() {
         this.app = express_1.default();
         this.config();
+        this.routes();
     }
     config() {
-        this.app.set('port', process.env.PORT || 4000);
-        this.app.use(morgan_1.default('dev')); // Para que muestre las url invocadas
+        return __awaiter(this, void 0, void 0, function* () {
+            this.app.set('port', process.env.PORT || 3000);
+            this.app.use(express_1.default.json()); // para que nuestro servidor entienda
+            // los formatos json desde clientes
+            this.app.use(morgan_1.default('dev')); // Para que muestre las url invocadas
+        });
     }
     routes() {
+        this.app.use('/futbol', futbolRoutes_1.futbolRoutes);
+        this.app.use('/prefijo', pruebaRoutes_1.pruebaRoutes);
+        this.app.use('/id', identificacionRoutes_1.identificacionRoutes);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,31 @@
 import express from 'express'
 import morgan from 'morgan'
+
+import { pruebaRoutes } from './routes/pruebaRoutes'
+import { futbolRoutes } from './routes/futbolRoutes'
+import { identificacionRoutes } from './routes/identificacionRoutes'
+import { db } from './database/database'
+
 class Server {
     private app: express.Application
     constructor(){
         this.app = express()
         this.config()
+        this.routes()
     }
-    config(){
-        this.app.set('port', process.env.PORT || 4000)
+    private async config(){
+
+        this.app.set('port', process.env.PORT || 3000)
+
+        this.app.use(express.json()) // para que nuestro servidor entienda
+        // los formatos json desde clientes
         this.app.use(morgan('dev'))  // Para que muestre las url invocadas
     }
-    routes(){
 
+    private routes(){
+        this.app.use('/futbol', futbolRoutes)
+        this.app.use('/prefijo', pruebaRoutes)
+        this.app.use('/id', identificacionRoutes)
     }
     start(){
         this.app.listen(this.app.get('port'), 
@@ -20,5 +34,6 @@ class Server {
         })
     }
 }
+
 const server = new Server()
 server.start()
